134f2131393ed46e80e74647b7174e33a0a98e74
Eldos <eldos.baktybekov@gmail.com>
2023-01-02 20:28:37 +0600

    :fire: Remove unused callback data

--- src/callback_data.py
+++ src/callback_data.py
@@ -3,21 +3,10 @@ from aiogram.utils.callback_data import CallbackData
 import models
 
 __all__ = (
-    'ChooseSubjectCallbackData',
     'SupportRequestDetailCallbackData',
     'CategoryDetailCallbackData',
     'ProductDetailCallbackData',
 )
-
-
-class ChooseSubjectCallbackData(CallbackData):
-
-    def __init__(self):
-        super().__init__('choose-subject', 'support_subject_id')
-
-    def parse(self, callback_data: str) -> models.ChooseSubjectCallbackData:
-        callback_data = super().parse(callback_data)
-        return {'support_subject_id': int(callback_data['support_subject_id'])}
 
 
 class SupportRequestDetailCallbackData(CallbackData):
