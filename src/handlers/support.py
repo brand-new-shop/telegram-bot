a25ace563be29795bea2492ff5230b7c28f45b00
Eldos <eldos.baktybekov@gmail.com>
2023-01-02 20:32:08 +0600

    :children_crossing: Improve the way of ticket's creation

--- src/handlers/support.py
+++ src/handlers/support.py
@@ -3,23 +3,32 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.types import Message, ContentType, CallbackQuery
 
-import exceptions
 import models
-from models import SupportRequestCreate
+from callback_data import SupportRequestDetailCallbackData
+from filters import MessageLengthFilter
 from services.api import SupportAPIClient, ShopInfoAPIClient
 from shortcuts import answer_views, edit_message_by_view
+from states import NewSupportRequestStates
 from views import (
     SupportMenuView,
-    SupportRequestCreatedView,
-    ChooseSubjectView,
+    SupportTicketCreatedView,
+    RequireTicketIssueView,
     SupportRequestsListView,
     SupportRequestDetailView,
     AcceptSupportRulesView,
+    RequireTicketSubjectView,
 )
-from states import NewSupportRequestStates
-from callback_data import ChooseSubjectCallbackData, SupportRequestDetailCallbackData
 
 __all__ = ('register_handlers',)
+
+
+# Validators
+async def on_support_ticket_issue_length_too_long(message: Message) -> None:
+    await message.answer('Issue text is too long (4096 characters maximum)')
+
+
+async def on_support_ticket_subject_length_too_long(message: Message) -> None:
+    await message.answer('Subject is too long (64 characters maximum)')
 
 
 async def on_support_request_detail(
@@ -27,63 +36,68 @@ async def on_support_request_detail(
         support_api_client: SupportAPIClient,
         callback_data: models.SupportRequestDetailCallbackData,
 ):
-    support_request = await support_api_client.get_request_by_id(callback_data['support_request_id'])
+    support_request = await support_api_client.get_ticket_by_id(callback_data['support_request_id'])
     view = SupportRequestDetailView(support_request)
     await edit_message_by_view(callback_query.message, view)
     await callback_query.answer()
 
 
 async def on_my_support_requests_list(message: Message, support_api_client: SupportAPIClient):
-    support_requests = await support_api_client.get_requests_by_user_telegram_id(message.from_user.id)
+    support_requests = await support_api_client.get_user_tickets(message.from_user.id)
     view = SupportRequestsListView(support_requests)
     await answer_views(message, view)
 
 
-async def on_input_support_request_issue(
+async def on_support_ticket_issue_input(
         message: Message,
-        support_api_client: SupportAPIClient,
         state: FSMContext,
+        support_api_client: SupportAPIClient,
 ) -> None:
     state_data = await state.get_data()
-    await state.finish()
-    subject_id = state_data['support_subject_id']
-    support_request_create_dto = SupportRequestCreate(
-        user_telegram_id=message.from_user.id,
+    ticket_create = models.SupportTicketCreate(
         issue=message.text,
-        subject_id=subject_id,
+        subject=state_data['subject'],
+        user_telegram_id=message.from_user.id,
     )
-    created_support_request = await support_api_client.create_request(support_request_create_dto)
-    await answer_views(message, SupportRequestCreatedView(created_support_request.id))
+    await state.finish()
+    created_support_ticket = await support_api_client.create_ticket(ticket_create)
+    await answer_views(message, SupportTicketCreatedView(created_support_ticket.id))
 
 
-async def on_choose_support_subject(
-        callback_query: CallbackQuery,
-        callback_data: models.ChooseSubjectCallbackData,
-        state: FSMContext,
-) -> None:
-    await state.update_data(support_subject_id=callback_data['support_subject_id'])
+async def on_support_ticket_subject_input(message: Message, state: FSMContext) -> None:
+    await state.update_data(subject=message.text)
     await NewSupportRequestStates.issue.set()
-    await callback_query.message.edit_text('📋 Describe your problem/question')
-    await callback_query.answer()
+    await answer_views(message, RequireTicketIssueView())
 
 
-async def on_new_support_request(message: Message, support_api_client: SupportAPIClient) -> None:
-    subjects = await support_api_client.get_subjects()
-    await NewSupportRequestStates.subject.set()
-    await answer_views(message, ChooseSubjectView(subjects))
-
-
-async def on_support_rules_were_read(message: Message) -> None:
-    await answer_views(message, SupportMenuView())
-
-
-async def on_support_menu(message: Message, shop_info_api_client: ShopInfoAPIClient) -> None:
+async def on_new_support_request(message: Message, shop_info_api_client: ShopInfoAPIClient) -> None:
     support_rules = await shop_info_api_client.get_support_rules_info()
     view = AcceptSupportRulesView(support_rules)
     await answer_views(message, view)
 
 
+async def on_support_rules_were_read(message: Message) -> None:
+    await NewSupportRequestStates.subject.set()
+    await answer_views(message, RequireTicketSubjectView())
+
+
+async def on_support_menu(message: Message) -> None:
+    await answer_views(message, SupportMenuView())
+
+
 def register_handlers(dispatcher: Dispatcher) -> None:
+    # Validators
+    dispatcher.register_message_handler(
+        on_support_ticket_subject_length_too_long,
+        ~MessageLengthFilter(max_length=64),
+        state=NewSupportRequestStates.subject,
+    )
+    dispatcher.register_message_handler(
+        on_support_ticket_issue_length_too_long,
+        ~MessageLengthFilter(max_length=4096),
+        state=NewSupportRequestStates.issue,
+    )
+
     dispatcher.register_message_handler(
         on_support_rules_were_read,
         Text('✅ I did'),
@@ -104,13 +118,13 @@ def register_handlers(dispatcher: Dispatcher) -> None:
         Text('📋 Submit New Ticket'),
         state='*',
     )
-    dispatcher.register_callback_query_handler(
-        on_choose_support_subject,
-        ChooseSubjectCallbackData().filter(),
+    dispatcher.register_message_handler(
+        on_support_ticket_subject_input,
+        content_types=ContentType.TEXT,
         state=NewSupportRequestStates.subject,
     )
     dispatcher.register_message_handler(
-        on_input_support_request_issue,
+        on_support_ticket_issue_input,
         content_types=ContentType.TEXT,
         state=NewSupportRequestStates.issue
     )
