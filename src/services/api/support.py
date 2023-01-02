b952cda26e28eee0e50932109c8d37c35cf047c6
Eldos <eldos.baktybekov@gmail.com>
2023-01-02 20:26:21 +0600

    :alien: Change routes for tickets creation

--- src/services/api/support.py
+++ src/services/api/support.py
@@ -14,31 +14,8 @@ class SupportAPIClient:
     def __init__(self, base_url: str):
         self.__base_url = base_url
 
-    async def get_subjects(self) -> tuple[models.SupportSubject, ...]:
-        async with httpx.AsyncClient(base_url=self.__base_url) as client:
-            response = await client.get('/support/')
-        if response.status_code != 200:
-            raise exceptions.ServerAPIError(f'Unexpected status code "{response.status_code}"')
-        try:
-            response_json = response.json()
-        except json.JSONDecodeError:
-            raise exceptions.ServerAPIError('Unable to parse response JSON')
-        return parse_obj_as(tuple[models.SupportSubject, ...], response_json)
-
-    async def get_subject_by_id(self, support_subject_id: int) -> models.SupportSubject:
-        url = f'/support/{support_subject_id}/'
-        async with httpx.AsyncClient(base_url=self.__base_url) as client:
-            response = await client.post(url)
-        if response.status_code != 200:
-            raise exceptions.ServerAPIError(f'Unexpected status code "{response.status_code}"')
-        try:
-            response_json = response.json()
-        except json.JSONDecodeError:
-            raise exceptions.ServerAPIError('Unable to parse response JSON')
-        return models.SupportSubject.parse_obj(response_json())
-
-    async def get_request_by_id(self, support_request_id: int) -> models.SupportRequest:
-        url = f'/support/requests/{support_request_id}/'
+    async def get_ticket_by_id(self, ticket_id: int) -> models.SupportRequest:
+        url = f'/users/tickets/{ticket_id}/'
         async with httpx.AsyncClient(base_url=self.__base_url) as client:
             response = await client.get(url)
         if response.status_code != 200:
@@ -49,8 +26,8 @@ class SupportAPIClient:
             raise exceptions.ServerAPIError('Unable to parse response JSON')
         return models.SupportRequest.parse_obj(response_json)
 
-    async def get_requests_by_user_telegram_id(self, user_telegram_id: int) -> tuple[models.SupportRequestPreview, ...]:
-        url = f'/support/requests/users/telegram-id/{user_telegram_id}/'
+    async def get_user_tickets(self, telegram_id: int) -> tuple[models.SupportTicketPreview, ...]:
+        url = f'/users/telegram-id/{telegram_id}/tickets/'
         async with httpx.AsyncClient(base_url=self.__base_url) as client:
             response = await client.get(url)
         if response.status_code != 200:
@@ -59,11 +36,11 @@ class SupportAPIClient:
             response_json = response.json()
         except json.JSONDecodeError:
             raise exceptions.ServerAPIError('Unable to parse response JSON')
-        return parse_obj_as(tuple[models.SupportRequestPreview, ...], response_json)
+        return parse_obj_as(tuple[models.SupportTicketPreview, ...], response_json)
 
-    async def create_request(self, support_request_create: models.SupportRequestCreate) -> models.SupportRequestCreated:
-        url = f'/support/requests/users/telegram-id/{support_request_create.user_telegram_id}/'
-        request_json = support_request_create.dict(include={'issue', 'subject_id'})
+    async def create_ticket(self, support_ticket_create: models.SupportTicketCreate) -> models.SupportTicketCreated:
+        url = f'/users/telegram-id/{support_ticket_create.user_telegram_id}/tickets/'
+        request_json = support_ticket_create.dict(include={'issue', 'subject'})
         async with httpx.AsyncClient(base_url=self.__base_url) as client:
             response = await client.post(url, json=request_json)
         if response.status_code != 201:
@@ -72,4 +49,4 @@ class SupportAPIClient:
             response_json = response.json()
         except json.JSONDecodeError:
             raise exceptions.ServerAPIError('Unable to parse response JSON')
-        return models.SupportRequestCreated.parse_obj(response_json)
+        return models.SupportTicketCreated.parse_obj(response_json)
