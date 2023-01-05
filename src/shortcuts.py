8d749b75017b425cc8fc59e2287630801f7e7c09
Eldos <eldos.baktybekov@gmail.com>
2023-01-05 15:05:45 +0600

    :sparkles: Add support of media groups in views

--- src/shortcuts.py
+++ src/shortcuts.py
@@ -1,4 +1,6 @@
 import contextlib
+import logging
+import traceback
 
 from aiogram.types import Message
 from aiogram.utils.exceptions import TelegramAPIError
@@ -10,8 +12,14 @@ __all__ = ('answer_views', 'edit_message_by_view')
 
 async def answer_views(message: Message, *views: View):
     for view in views:
-        with contextlib.suppress(TelegramAPIError):
-            await message.answer(text=view.get_text(), reply_markup=view.get_reply_markup())
+        media_group = view.get_media_group()
+        try:
+            if media_group is not None:
+                await message.answer_media_group(media_group)
+            else:
+                await message.answer(text=view.get_text(), reply_markup=view.get_reply_markup())
+        except TelegramAPIError:
+            logging.error(traceback.format_exc())
 
 
 async def edit_message_by_view(message: Message, view: View):
--- src/views/base.py
+++ src/views/base.py
@@ -1,4 +1,4 @@
-from aiogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup, ForceReply, ReplyKeyboardRemove
+from aiogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup, ForceReply, ReplyKeyboardRemove, MediaGroup
 
 __all__ = ('View',)
 
@@ -6,9 +6,13 @@ __all__ = ('View',)
 class View:
     text: str | None = None
     reply_markup: InlineKeyboardMarkup | ReplyKeyboardMarkup | ForceReply | ReplyKeyboardRemove | None = None
+    media_group: MediaGroup | None = None
 
     def get_text(self) -> str | None:
         return self.text
 
     def get_reply_markup(self) -> InlineKeyboardMarkup | ReplyKeyboardMarkup | ForceReply | ReplyKeyboardRemove | None:
         return self.reply_markup
+
+    def get_media_group(self) -> MediaGroup:
+        return self.media_group
