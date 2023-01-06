6c2c9d11f822453947662616b2c8de0613b1b71e
Eldos <eldos.baktybekov@gmail.com>
2023-01-06 13:04:41 +0600

    :technologist: Add `in developing` alert handler

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,10 +1,10 @@
 from aiogram import Dispatcher
 
-from . import start, remove_message, balance, profile, support, products
+from . import start, remove_message, balance, profile, support, products, dev
 
 __all__ = ('register_handlers',)
 
 
 def register_handlers(dispatcher: Dispatcher) -> None:
-    for module in (remove_message, start, balance, profile, support, products):
+    for module in (remove_message, start, balance, profile, support, products, dev):
         module.register_handlers(dispatcher)
