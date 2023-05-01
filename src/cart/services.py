d42a0e170f6f01c0b68e829971f713b575170695
Eldos <eldos.baktybekov@gmail.com>
2023-05-01 19:06:54 +0600

    :recycle: Refactor API clients

--- src/cart/services.py
+++ src/cart/services.py
@@ -1,41 +1,39 @@
 import models
-from core.services import APIClient, safely_decode_response_json
+from core.services import BaseAPIClient, safely_decode_response_json
 
 __all__ = ('CartsAPIClient',)
 
 
-class CartsAPIClient:
+class CartsAPIClient(BaseAPIClient):
 
-    def __init__(self, api_client: APIClient):
-        self._api_client = api_client
-
-    async def create_cart_product(self, telegram_id: int, product_id: int, quantity: int) -> models.CartProduct:
+    async def create_cart_product(
+            self,
+            telegram_id: int,
+            product_id: int,
+            quantity: int,
+    ) -> models.CartProduct:
         url = f'/carts/users/{telegram_id}/'
         request_body = {'product_id': product_id, 'quantity': quantity}
-        async with self._api_client.closing_http_client_factory() as client:
-            response = await client.post(url, json=request_body)
+        response = await self._http_client.post(url, json=request_body)
         response_body = safely_decode_response_json(response)
         return models.CartProduct.parse_obj(response_body)
 
     async def delete_card_product(self, cart_product_id: int):
         url = f'/carts/{cart_product_id}/'
-        async with self._api_client.closing_http_client_factory() as client:
-            response = await client.delete(url)
+        response = await self._http_client.delete(url)
 
     async def get_card_product(self, cart_product_id: int):
         url = f'/carts/{cart_product_id}/'
-        async with self._api_client.closing_http_client_factory() as client:
-            response = await client.get(url)
+        response = await self._http_client.get(url)
 
     async def get_user_card_products(self, telegram_id: int):
         url = f'/carts/users/{telegram_id}/'
-        async with self._api_client.closing_http_client_factory() as client:
-            response = await client.get(url)
+        response = await self._http_client.get(url)
         response_body = safely_decode_response_json(response)
         return models.CartProduct.parse_obj(response_body)
 
-    async def update_card_product_quantity(self, cart_product_id: int, quantity: int):
+    async def update_card_product_quantity(self, cart_product_id: int,
+                                           quantity: int):
         url = f'/carts/{cart_product_id}/'
         request_body = {'quantity': quantity}
-        async with self._api_client.closing_http_client_factory() as client:
-            response = await client.patch(url, json=request_body)
+        response = await self._http_client.patch(url, json=request_body)
