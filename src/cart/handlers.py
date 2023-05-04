4b4cfcd32bfc7551af8c1a3f017000a1f294a0c6
Eldos <eldos.baktybekov@gmail.com>
2023-05-04 17:59:11 +0600

    :poop: Add change cart product quantity handler

--- src/cart/handlers.py
+++ src/cart/handlers.py
@@ -7,14 +7,42 @@ from cart.views import CartView
 from cart import models
 from cart.callback_data import ChangeProductQuantityInCartCallbackData
 from core.services import HTTPClientFactory
-from core.shortcuts import answer_views
+from core.shortcuts import answer_views, edit_message_by_view
+from products.services import ProductsAPIClient
 
 
 async def on_change_product_quantity(
         callback_query: CallbackQuery,
-        callback_data: models.HasProductIdAndQuantityTypedDict,
+        callback_data: models.ChangeProductQuantityInCartTypedDict,
+        closing_http_client_factory: HTTPClientFactory,
 ) -> None:
-    print(callback_data)
+    cart_product_id = callback_data['cart_product_id']
+    quantity = callback_data['quantity']
+    product_id = callback_data['product_id']
+
+    async with closing_http_client_factory() as http_client:
+        cart_api_client = CartAPIClient(http_client)
+        products_api_client = ProductsAPIClient(http_client)
+        product = await products_api_client.get_product_by_id(product_id)
+        cart_product = await cart_api_client.get_cart_product(cart_product_id)
+        cart_product_quantity_change = cart_product.quantity - quantity
+        if cart_product_quantity_change > product.stocks_count:
+            await callback_query.answer('Not enough stocks', show_alert=True)
+            return
+        if quantity <= 0:
+            await cart_api_client.delete_cart_product(cart_product_id)
+        else:
+            await cart_api_client.update_cart_product_quantity(
+                cart_product_id, quantity
+            )
+            await callback_query.answer(
+                'You have updated the quantity of the item in the cart'
+            )
+        cart_products = await cart_api_client.get_cart_products(
+            telegram_id=callback_query.from_user.id,
+        )
+    view = CartView(cart_products)
+    await edit_message_by_view(callback_query.message, view)
 
 
 async def on_show_cart(
