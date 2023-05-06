9e12743f8c8843b06d0cc8d28be819610992a032
Eldos <eldos.baktybekov@gmail.com>
2023-05-06 20:24:24 +0600

    :sparkles: Add handler to pay order via balance

--- src/cart/handlers.py
+++ src/cart/handlers.py
@@ -3,12 +3,80 @@ from aiogram.dispatcher.filters import Text
 from aiogram.types import Message, CallbackQuery
 
 from cart.services import CartAPIClient
-from cart.views import CartView
+from cart.views import CartView, PaymentMethodsView
 from cart import models
 from cart.callback_data import ChangeProductQuantityInCartCallbackData
 from core.services import HTTPClientFactory
 from core.shortcuts import answer_views, edit_message_by_view
 from products.services import ProductsAPIClient
+from users.services import UsersAPIClient
+
+
+async def on_select_balance_as_payment_method(
+        callback_query: CallbackQuery,
+        closing_http_client_factory: HTTPClientFactory,
+) -> None:
+    async with closing_http_client_factory() as http_client:
+        users_api_client = UsersAPIClient(http_client)
+        cart_api_client = CartAPIClient(http_client)
+        user = await users_api_client.get_by_telegram_id(
+            telegram_id=callback_query.from_user.id,
+        )
+        cart_products = await cart_api_client.get_cart_products(
+            telegram_id=callback_query.from_user.id,
+        )
+        if not cart_products:
+            await callback_query.answer(
+                'Your shopping cart is empty',
+                show_alert=True,
+            )
+            return
+
+        cart_total_cost = sum(
+            cart_product.quantity * cart_product.product_price
+            for cart_product in cart_products
+        )
+        if cart_total_cost > user.balance:
+            missing_amount = cart_total_cost - user.balance
+            text = (
+                'You do not have enough balance on your account.'
+                f' you need an additional ${missing_amount}'
+                ' to complete the order'
+            )
+            await callback_query.answer(text, show_alert=True)
+            return
+
+        order = await cart_api_client.create_order(
+            telegram_id=callback_query.from_user.id,
+            payment_method='balance',
+        )
+    await callback_query.message.edit_text('Order successfully created')
+
+
+async def on_start_order_processing(
+        callback_query: CallbackQuery,
+        closing_http_client_factory: HTTPClientFactory,
+) -> None:
+    async with closing_http_client_factory() as http_client:
+        cart_api_client = CartAPIClient(http_client)
+        products_api_client = ProductsAPIClient(http_client)
+
+        cart_products = await cart_api_client.get_cart_products(
+            telegram_id=callback_query.from_user.id,
+        )
+        products = []
+        for cart_product in cart_products:
+            products.append(
+                await products_api_client.get_product_by_id(
+                    product_id=cart_product.product_id,
+                )
+            )
+    balance_only_products = [
+        product for product in products
+        if product.is_balance_only
+    ]
+    view = PaymentMethodsView(balance_only_products)
+    await answer_views(callback_query.message, view)
 
 
 async def on_empty_cart(
@@ -81,6 +149,16 @@ async def on_show_cart(
 
 
 def register_handlers(dispatcher: Dispatcher) -> None:
+    dispatcher.register_callback_query_handler(
+        on_select_balance_as_payment_method,
+        Text('pay-via-balance'),
+        state='*',
+    )
+    dispatcher.register_callback_query_handler(
+        on_start_order_processing,
+        Text('start-order-processing'),
+        state='*',
+    )
     dispatcher.register_callback_query_handler(
         on_empty_cart,
         Text('empty-cart'),
