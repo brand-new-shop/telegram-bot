a16c7f6f372fb2fdc91acaeb574dcf5d9eb213af
Eldos <eldos.baktybekov@gmail.com>
2023-05-06 18:00:53 +0600

    :alien: Refactor profile menu

--- src/users/handlers.py
+++ src/users/handlers.py
@@ -1,5 +1,3 @@
-import asyncio
-
 from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import CommandStart
@@ -8,33 +6,33 @@ from aiogram.types import CallbackQuery, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import Message
 from aiogram.utils.exceptions import TelegramAPIError
 
+from cart.services import CartAPIClient
 from core import exceptions
 from core.services import HTTPClientFactory
 from core.shortcuts import answer_views
 from info.services import ShopInfoAPIClient
 from info.views import ShopInfoView
-from users.models import UserProfileDTO
 from users.services import UsersAPIClient
 from users.views import MenuView, AcceptRulesView, ProfileView
 
 __all__ = ('register_handlers',)
 
 
-async def on_profile(message: Message, users_api_client: UsersAPIClient):
-    orders, orders_statistics = await asyncio.gather(
-        users_api_client.get_orders_by_telegram_id(message.from_user.id,
-                                                   limit=10, offset=0),
-        users_api_client.get_orders_statistics(message.from_user.id),
-    )
-    user_profile_dto = UserProfileDTO(
-        telegram_id=message.from_user.id,
-        username=message.from_user.username,
-        purchases_total_count=orders_statistics.orders_count,
-        purchases_total_price=orders_statistics.orders_total_price,
-        last_purchases=orders,
-    )
-    view = ProfileView(user_profile_dto)
-    await answer_views(message, view)
+async def on_profile(
+        message: Message,
+        closing_http_client_factory: HTTPClientFactory,
+):
+    async with closing_http_client_factory() as http_client:
+        cart_api_client = CartAPIClient(http_client)
+        orders_statistics = await cart_api_client.get_orders_statistics(
+            telegram_id=message.from_user.id
+        )
+        view = ProfileView(
+            telegram_id=message.from_user.id,
+            username=message.from_user.username,
+            orders_statistics=orders_statistics
+        )
+        await answer_views(message, view)
 
 
 async def on_remove_message(callback_query: CallbackQuery):
